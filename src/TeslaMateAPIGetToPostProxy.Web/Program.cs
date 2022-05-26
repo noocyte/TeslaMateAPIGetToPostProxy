a8d97994ba5c4adf94b6ae38491596993ef98ca7
Jarle Nygård <jny@proactimasolutions.com>
2022-05-26 12:52:22 +0200

    Updated code to wake up car before charge command

--- src/TeslaMateAPIGetToPostProxy.Web/Program.cs
+++ src/TeslaMateAPIGetToPostProxy.Web/Program.cs
@@ -20,23 +20,56 @@ app.MapGet("/test", async (IHttpClientFactory factory) =>
 
     var response = await client.GetAsync("/api/v1/cars/1/command");
     var body = await response.Content.ReadAsStringAsync();
-    return body;
+    return Results.Ok(body);
 });
-app.MapGet("/trunk", async (string token, IHttpClientFactory factory) =>
+
+app.MapGet("/charge", async (string token, IHttpClientFactory factory, ILogger<Program> logger) =>
 {
     var client = factory.CreateClient(clientName);
     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{token}");
-    var response = await client.PostAsJsonAsync("/api/v1/cars/1/command/actuate_trunk", new { which = "rear" });
-    var body = await response.Content.ReadAsStringAsync();
-    return body;
-});
-app.MapGet("/charge", async (string token, IHttpClientFactory factory) =>
-{
-    var client = factory.CreateClient(clientName);
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{token}");
-    var response = await client.PostAsJsonAsync("/api/v1/cars/1/command/charge_port_door_open", new { });
-    var body = await response.Content.ReadAsStringAsync();
-    return body;
+
+    var isAwake = false;
+    var counter = 0;
+    do
+    {
+        counter++;
+        var wakeResponse = await client.PostAsJsonAsync("/api/v1/cars/1/wake_up", new { });
+        if (!wakeResponse.IsSuccessStatusCode)
+        {
+            var wakeResponseBody = wakeResponse.Content.ReadAsStringAsync();
+            logger.LogWarning("Call to wakeup failed: {status}, message: {message}", wakeResponse.StatusCode, wakeResponseBody);
+            continue;
+        }
+
+        var currentStatus = await wakeResponse.Content.ReadFromJsonAsync<TeslaAPIResponse>();
+        if (currentStatus == null)
+        {
+            logger.LogError("currentStatus is null");
+            continue;
+        }
+
+        isAwake = currentStatus.response.state.Equals("online", StringComparison.OrdinalIgnoreCase);
+
+        if (!isAwake)
+        {
+            logger.LogInformation("Waiting 500ms - counter: {counter}", counter);
+            await Task.Delay(500);
+        }
+
+    } while (!isAwake && counter <= 10);
+
+    if (!isAwake || counter >= 10)
+    {
+        logger.LogError("Failed to wakeup car, tried {counter} times", counter);
+        return Results.Problem(detail: "Wakeup failed", statusCode: 500);
+    }
+
+    var chargeResponse = await client.PostAsJsonAsync("/api/v1/cars/1/command/charge_port_door_open", new { });
+    var body = await chargeResponse.Content.ReadAsStringAsync();
+    logger.LogInformation("Response from Charge Command: {response}", body);
+
+    return Results.Ok(body);
 });
 
 app.Run("http://*:3000");
+
